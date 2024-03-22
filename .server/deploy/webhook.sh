ecdd9d46292ced2f7832b6beddd9f6fa4b763da4
李款 <likuan@baixing.com>
2024-03-22 14:41:42 +0800

    拉取最新代码前的commit 2024-03-22 14:41:42

--- .server/deploy/webhook.sh
+++ .server/deploy/webhook.sh
@@ -8,6 +8,6 @@ git branch
 git add .
 git commit -m "拉取最新代码前的commit $(date +'%Y-%m-%d %H:%M:%S')"
 
-# echo "开始拉取"
-# git pull origin master
-# echo "拉取完成"
+echo "开始拉取"
+git pull origin master
+echo "拉取完成"
